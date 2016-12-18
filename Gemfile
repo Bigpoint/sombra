d936ed0bcf8a07c0181aa70678648d8bc81de8c1
Nils Bartels <n.bartels@bigpoint.net>
2016-12-19 00:21:15 +0100

    [ADD] basic integration tests; [CHANGE] only allow 1 mongodb host in config (futureme might hate me for this);

--- Gemfile
+++ Gemfile
@@ -18,9 +18,6 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
 # gem dedicated to sombra
 gem 'unicorn', '~> 5.2.0'
 gem 'unicorn-worker-killer', '~> 0.4.4'
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-   allow do
-     origins 'example.com'
+#Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'example.com'
 
-     resource '*',
-       headers: :any,
-       methods: [:get, :post, :put, :delete]
-   end
-end
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :delete]
+#   end
+#end
